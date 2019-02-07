411a756bb3c0bedbcc891eba46f5cb6a21b35e8f
LiulietLee <lee.liuliet@hotmail.com>
2019-02-08 00:07:28 +0800

    remove abstract metal view

--- mikutap/MetalView/MetalView.swift
+++ mikutap/MetalView/MetalView.swift
@@ -8,12 +8,20 @@
 
 import MetalKit
 
-class MetalView: AbstractMetalView {
+class MetalView: MTKView {
     
     private var animation = [AbstractAnimation]()
     
+    private var commandQueue: MTLCommandQueue?
+    private var rps: MTLRenderPipelineState?
+    private var semaphore: DispatchSemaphore!
+
     private func commonInit() {
-        animation.append(CircleAnimation(device: device!))
+        semaphore = DispatchSemaphore(value: 3)
+        device = MTLCreateSystemDefaultDevice()!
+        commandQueue = device!.makeCommandQueue()
+        
+//        animation.append(CircleAnimation(device: device!))
         animation.append(XAnimation(device: device!))
     }
     
@@ -34,24 +42,24 @@ class MetalView: AbstractMetalView {
             
             if let drawable = currentDrawable, let rpd = currentRenderPassDescriptor {
                 rpd.colorAttachments[0].clearColor = MTLClearColorMake(0.1, 0.5, 0.7, 1.0)
-                
+
                 let commandBuffer = commandQueue!.makeCommandBuffer()
-                
-                var removeList = [Int]()
-                for i in 0..<animations.count {
-                    if !animation[i].setCommandEncoder(cb: commandBuffer!, rpd: rpd) {
-                        removeList.append(i)
-                        continue
+                if animation.isEmpty {
+                    let commandEncoder = commandBuffer?.makeRenderCommandEncoder(descriptor: rpd)
+                    commandEncoder?.endEncoding()
+                } else {
+                    var removeList = [Int]()
+                    for i in 0..<animation.count {
+                        if !animation[i].setCommandEncoder(cb: commandBuffer!, rpd: rpd) {
+                            removeList.append(i)
+                            continue
+                        }
+                        rpd.colorAttachments[0].loadAction = .load
                     }
-                    rpd.colorAttachments[0].loadAction = .load
-                }
-                for i in removeList.reversed() {
-                    animation.remove(at: i)
-                }
-                
-                // TODO: - animation 数组清空后有概率出现闪屏问题
-                if animations.isEmpty {
-                    animation.append(PolygonAnimation(device: device!))
+                    for i in removeList.reversed() {
+                        animation.remove(at: i)
+                    }
+                    // TODO: - animation 数组清空后有概率出现闪屏问题
                 }
                 
                 commandBuffer?.present(drawable)
