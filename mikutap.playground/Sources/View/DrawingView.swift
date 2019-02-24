cc8649c40ea819d3e5faff03f43061138410609e
LiulietLee <lee.liuliet@hotmail.com>
2019-02-24 17:43:51 +0800

    add axis

--- mikutap.playground/Sources/View/DrawingView.swift
+++ mikutap.playground/Sources/View/DrawingView.swift
@@ -9,10 +9,103 @@ class DrawingView: MTKView {
     private var rps: MTLRenderPipelineState?
     private var semaphore: DispatchSemaphore!
 
+    private func axisInit() {
+        let width: CGFloat = 2.0
+        
+        let xaxis = UILabel()
+        xaxis.backgroundColor = .white
+        addSubview(xaxis)
+        
+        xaxis.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            xaxis.heightAnchor.constraint(equalToConstant: width),
+            xaxis.widthAnchor.constraint(equalTo: widthAnchor),
+            xaxis.centerXAnchor.constraint(equalTo: centerXAnchor),
+            xaxis.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+        
+        let yaxis = UILabel()
+        yaxis.backgroundColor = .white
+        addSubview(yaxis)
+        
+        yaxis.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            yaxis.widthAnchor.constraint(equalToConstant: width),
+            yaxis.heightAnchor.constraint(equalTo: heightAnchor),
+            yaxis.centerXAnchor.constraint(equalTo: centerXAnchor),
+            yaxis.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+        
+        let xlabelr = UILabel()
+        xlabelr.text = "  x  \n\n 1.0"
+        xlabelr.numberOfLines = 3
+        xlabelr.textColor = .white
+        xlabelr.sizeToFit()
+        addSubview(xlabelr)
+        
+        xlabelr.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            xlabelr.rightAnchor.constraint(equalTo: rightAnchor),
+            xlabelr.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+
+        let xlabell = UILabel()
+        xlabell.text = "\n\n -1.0"
+        xlabell.numberOfLines = 3
+        xlabell.textColor = .white
+        xlabell.sizeToFit()
+        addSubview(xlabell)
+        
+        xlabell.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            xlabell.leftAnchor.constraint(equalTo: leftAnchor),
+            xlabell.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+        
+        let ylabelu = UILabel()
+        ylabelu.text = "  y    1.0"
+        ylabelu.textColor = .white
+        ylabelu.sizeToFit()
+        addSubview(ylabelu)
+        
+        ylabelu.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            ylabelu.topAnchor.constraint(equalTo: topAnchor),
+            ylabelu.centerXAnchor.constraint(equalTo: centerXAnchor)
+        ])
+
+        let ylabelb = UILabel()
+        ylabelb.text = "         -1.0"
+        ylabelb.textColor = .white
+        ylabelb.sizeToFit()
+        addSubview(ylabelb)
+        
+        ylabelb.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            ylabelb.bottomAnchor.constraint(equalTo: bottomAnchor),
+            ylabelb.centerXAnchor.constraint(equalTo: centerXAnchor)
+        ])
+        
+        let clabel = UILabel()
+        clabel.text = "0"
+        clabel.textColor = .white
+        clabel.sizeToFit()
+        addSubview(clabel)
+        
+        clabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            clabel.centerXAnchor.constraint(equalTo: centerXAnchor, constant: 20.0),
+            clabel.centerYAnchor.constraint(equalTo: centerYAnchor, constant: 20.0)
+        ])
+    }
+    
     private func commonInit() {
         semaphore = DispatchSemaphore(value: 3)
         device = MTLCreateSystemDefaultDevice()!
         commandQueue = device!.makeCommandQueue()
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        axisInit()
     }
     
     override init(frame frameRect: CGRect, device: MTLDevice?) {
