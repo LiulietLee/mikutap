309493f258cd224abe0acd1592893c7b9179a9fa
LiulietLee <lee.liuliet@hotmail.com>
2019-03-24 00:04:06 +0800

    typo

--- mikutap.playgroundbook/Contents/Chapters/Chapter1.playgroundchapter/Pages/Introduction.playgroundpage/Contents.swift
+++ mikutap.playgroundbook/Contents/Chapters/Chapter1.playgroundchapter/Pages/Introduction.playgroundpage/Contents.swift
@@ -3,8 +3,9 @@
  # Mikutap
  
  As a developer who loves Hatsune Miku, I was attracted to it when I first saw Mikutap on the web.
- 
- Mikutap is a web music game originally made by *daniwell*. The entire view in the game is divided into a number of neat little squares, each of which corresponds to a pitch and a fifty-tone in Japanese. Touching a square with your finger will send out a corresponding tone and lyrics, and you can form a song melody by sliding continuously. Coupled with a nice set of fingertip animations, it's just awesome.
+  
+ Mikutap is a music game originally made by *daniwell* for web. The entire view in the game is divided into a number of neat little squares, each one corresponds to a pitch and a fifty-tone in Japanese. Touching a square with your finger will send out a corresponding tone and lyrics, and you can form a song melody by sliding continuously. Coupled with a nice set of fingertip animations, it’s just awesome.
+
  
  Swift is my favorite programming language, and Hatsune Miku is my favorite virtual singer. In order to make two happy things together, I wrote this playground. It is recommended to **turn on the volume** and **horizontal full screen**.
  
